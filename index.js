3a9fbe18d1200067b61c8fb2548a19f055d14fa5
Evgeny Pletnev <e.a.pletnev@gmail.com>
2020-11-28 19:13:45 +0000

    Created index.js file and added quotes

---
+++ index.js
@@ -0,0 +1,7 @@
+const quotes = [
+  "'Do you fall in love with boys or with girls?' I asked her. 'Sometimes boys,' she replied. 'Mostly souls.' ― Juansen Dizon",
+  "'Only by speaking out can we create lasting change. And that change begins with coming out.' ― DaShanne Stokes",
+  "'To all the young people who discover they are gay: Don't be afraid to come out of the closet right now.' ― Núria Añó",
+  "'I’ll be okay even if i don’t understand how i don’t want to be a girl, but also don’t want to be a man.' ― Courtney Carola",
+  "'Standing against discrimination for some while supporting discrimination against others hurts us all.' ― DaShanne Stokes" 
+];
