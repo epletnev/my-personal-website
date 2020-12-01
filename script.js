3753513bc5d08965c4816ccb3be80e003703bd85
Evgeny Pletnev <e.a.pletnev@gmail.com>
2020-12-01 22:08:06 +0000

    Added more quotes to js file

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const quotes = [
   "'Only by speaking out can we create lasting change. And that change begins with coming out.' ― DaShanne Stokes",
   "'To all the young people who discover they are gay: Don't be afraid to come out of the closet right now.' ― Núria Añó",
   "'I’ll be okay even if i don’t understand how i don’t want to be a girl, but also don’t want to be a man.' ― Courtney Carola",
-  "'Standing against discrimination for some while supporting discrimination against others hurts us all.' ― DaShanne Stokes" 
+  "'Standing against discrimination for some while supporting discrimination against others hurts us all.' ― DaShanne Stokes",
+    "'One day, progress being what it was, I hoped no one would have to have a big gay coming-out or a bisexual coming-out. It would just be what it was, and that would be that. But we weren't quite there yet.' ― S.E. Harmon",
+  "'Living in a world that is grossly opposed to who you are, the bravest and most radical thing to do is to be yourself. And being a part of the LGBT+ community is just that – brave.' ― Courtney Carola",
+  "'How heavenly it would be to live their lives free from the thought that they were being criticized, pointed out - their love for each other discussed as though it were some low vulgar thing.' ― Lilyan Brock",
+  "'Saying it's hard being straight is like complaining to the poor that it's difficult being wealthy.' ― DaShanne Stokes",
+  "'Pride is not an LGBT celebration, it's a human rights celebration - it's a celebration of equality - it's a celebration of inclusion - it's a celebration of acceptance.' ― Abhijit Naskar" 
 ];
