68ab53ec64d4b051e8b2ed1ff8a7b2d9ba7b6169
Evgeny Pletnev <e.a.pletnev@gmail.com>
2021-01-03 18:48:06 +0000

    Removed header from css

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const quotes = [
   "'To all the young people who discover they are gay: Don't be afraid to come out of the closet right now.' ― Núria Añó",
   "'I’ll be okay even if i don’t understand how i don’t want to be a girl, but also don’t want to be a man.' ― Courtney Carola",
   "'Standing against discrimination for some while supporting discrimination against others hurts us all.' ― DaShanne Stokes",
-    "'One day, progress being what it was, I hoped no one would have to have a big gay coming-out or a bisexual coming-out. It would just be what it was, and that would be that. But we weren't quite there yet.' ― S.E. Harmon",
+  "'One day, progress being what it was, I hoped no one would have to have a big gay coming-out or a bisexual coming-out. It would just be what it was, and that would be that. But we weren't quite there yet.' ― S.E. Harmon",
   "'Living in a world that is grossly opposed to who you are, the bravest and most radical thing to do is to be yourself. And being a part of the LGBTQ community is just that – brave.' ― Courtney Carola",
   "'How heavenly it would be to live their lives free from the thought that they were being criticized, pointed out - their love for each other discussed as though it were some low vulgar thing.' ― Lilyan Brock",
   "'Saying it's hard being straight is like complaining to the poor that it's difficult being wealthy.' ― DaShanne Stokes",
-  "'Pride is not an LGBTQ celebration, it's a human rights celebration - it's a celebration of equality - it's a celebration of inclusion - it's a celebration of acceptance.' ― Abhijit Naskar" 
+  "'Pride is not an LGBTQ celebration, it's a human rights celebration - it's a celebration of equality - it's a celebration of inclusion - it's a celebration of acceptance.' ― Abhijit Naskar"
 ];
 
 let slideIndex = 1;
@@ -29,7 +29,7 @@ function slider(n) {
   let i;
   const slides = document.getElementsByClassName("slide");
   const circles = document.getElementsByClassName("circle");
-if (n > slides.length) {
+  if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
@@ -45,7 +45,7 @@ if (n > slides.length) {
   circles[slideIndex - 1].className += " active";
 
 // New quote function
-    const randomNumber = Math.floor(Math.random() * quotes.length);
+  const randomNumber = Math.floor(Math.random() * quotes.length);
   document.getElementById("newQuote").innerHTML = quotes[randomNumber];
 }
 
